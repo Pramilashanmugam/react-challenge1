6b78acf9390f5cbab8c05d2332a8ab08046c01c5
Pramila Shanmugam <pramila.sl@gmail.com>
2024-09-04 10:26:35 +0000

    commented all the function in es7snippet.js

--- my-app/src/components/ES7Snippets.js
+++ my-app/src/components/ES7Snippets.js
@@ -1,51 +1,56 @@
 //rfce: importing react, create a function and export it
-import React from 'react'
+//import React from 'react'
 
-function ES7Snippets() {
-  return (
-    <div>ES7Snippets</div>
-  )
-}
+//function ES7Snippets() {
+//  return (
+//    <div>ES7Snippets</div>
+//  )
+//}
 
-export default ES7Snippets
+//export default ES7Snippets
+
+//-------------------------------------------------------
 
 //rafce: it is same as above with arrow function
-import React from 'react'
+//import React from 'react'
 
-const ES7Snippets = () => {
-  return (
-    <div>ES7Snippets</div>
-  )
-}
+//const ES7Snippets = () => {
+//  return (
+//    <div>ES7Snippets</div>
+//  )
+//}
 
-export default ES7Snippets
-
+//export default ES7Snippets
+//-------------------------------------------------------
 
 //rce :create and export class component
-import React, { Component } from 'react'
+//import React, { Component } from 'react'
 
-export class ES7Snippets extends Component {
-  render() {
-    return (
-      <div>ES7Snippets</div>
-    )
-  }
-}
+//export class ES7Snippets extends Component {
+//  render() {
+//    return (
+//      <div>ES7Snippets</div>
+//    )
+//  }
+//}
 
-export default ES7Snippets
+//export default ES7Snippets
+//-------------------------------------------------------
 
 //rconst :creates a constructor
 
-constructor(props) {
-  super(props)
+//constructor(props) {
+//  super(props)
 
-  this.state = {
-     first
-  }
-}
+//  this.state = {
+//     first
+//  }
+//}
+//-------------------------------------------------------
 
 //clg
-console.log(first)
+//console.log(first)
+//-------------------------------------------------------
 
 //clo
-console.log('first', first)
+//console.log('first', first)
