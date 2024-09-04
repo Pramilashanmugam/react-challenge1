8c4e4b0bad30f44bc022c1601eb49046150acfce
Pramila Shanmugam <pramila.sl@gmail.com>
2024-09-04 10:22:53 +0000

    es7 snippets file added

---
+++ my-app/src/components/ES7Snippets.js
@@ -0,0 +1,51 @@
+//rfce: importing react, create a function and export it
+import React from 'react'
+
+function ES7Snippets() {
+  return (
+    <div>ES7Snippets</div>
+  )
+}
+
+export default ES7Snippets
+
+//rafce: it is same as above with arrow function
+import React from 'react'
+
+const ES7Snippets = () => {
+  return (
+    <div>ES7Snippets</div>
+  )
+}
+
+export default ES7Snippets
+
+
+//rce :create and export class component
+import React, { Component } from 'react'
+
+export class ES7Snippets extends Component {
+  render() {
+    return (
+      <div>ES7Snippets</div>
+    )
+  }
+}
+
+export default ES7Snippets
+
+//rconst :creates a constructor
+
+constructor(props) {
+  super(props)
+
+  this.state = {
+     first
+  }
+}
+
+//clg
+console.log(first)
+
+//clo
+console.log('first', first)
